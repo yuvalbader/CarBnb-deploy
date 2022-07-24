ccce6fccb2e992b76b7a02bb4893716d7ec17924
Roniana11 <roniana11@gmail.com>
2022-07-24 11:05:17 +0300

    vehicles list no conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,7 @@
 import './App.css';
-import VehicleListContainer from './components/vehicles-list/VehiclesListContainer';
-import bmw from './images/bmw.jpg';
-import chevrolet from './images/chevrolet.jpg';
-import tesla from './images/tesla.jpg';
+
 function App() {
-  const vehicles = [
-    {
-      brand: 'BMW',
-      model: 'i8',
-      year: '2016',
-      rating: '4.91',
-      price: '200',
-      image: bmw,
-    },
-    {
-      brand: 'Tesla',
-      model: 'Model S',
-      year: '2022',
-      rating: '5.0',
-      price: '250',
-      image: chevrolet,
-    },
-    {
-      brand: 'Chevrolet',
-      model: 'Covert',
-      year: '2016',
-      rating: '4.95',
-      price: '115',
-      image: tesla,
-    },
-  ];
-  return (
-    <div className="App">
-      <VehicleListContainer vehicles={vehicles} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
