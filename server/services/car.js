16ea243c9d48af8e052a2844427c8f587e736c2f
ChenAzulai <ChenAzulai@users.noreply.github.com>
2022-08-02 18:20:38 +0300

    fix: conflict with main

--- server/services/car.js
+++ server/services/car.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const { Car } = require("../db/models");
-=======
-const { Car } = require("../db/models")
->>>>>>> 5361e5794ab0056f45232d39d636f2d34199bb05
 
 class CarsService {
   constructor() {}
@@ -15,23 +11,23 @@ class CarsService {
     {
       jeep: "https://resources.turo.com/f/81934/386x308/9bf274f19e/image_make_jeep-2x.jpg",
     },
-  ]
+  ];
 
   getAllCars = async () => {
-    return await Car.findAll()
-  }
+    return await Car.findAll();
+  };
 
   getCarById = async (carId) => {
-    return await Car.findByPk(carId)
-  }
+    return await Car.findByPk(carId);
+  };
 
   getCarByUserId = async (userId) => {
     return await Car.findAll({
       where: {
         user_id: userId,
       },
-    })
-  }
+    });
+  };
 
   addCar = async (car) => {
     return await Car.create({
@@ -45,44 +41,34 @@ class CarsService {
       user_id: car.user_id,
       type: car.type,
       location: car.location,
-    })
-  }
+    });
+  };
 
   updateCar = async (id, updateCar) => {
     return await Car.update(updateCar, {
       where: { id: id },
-    })
-  }
+    });
+  };
 
   deleteCar = async (carId) => {
     return await Car.destroy({
       where: { id: carId },
-    })
-  }
+    });
+  };
 
   deleteAllCars = async () => {
     return await Car.destroy({
       where: {},
       truncate: true,
-    })
-  }
-
-  getBrands = async () => {
-    return await Car.findAll({
-      attributes: ["brand"],
-      group: ["brand"],
-<<<<<<< HEAD
     });
   };
 
-  getUserIdByCarId = async (carId) => {
-    const car = await Car.findByPk(carId);
-    return car;
-  };
-=======
-    })
-  }
->>>>>>> 5361e5794ab0056f45232d39d636f2d34199bb05
+  // getBrands = async () => {
+  //   return await Car.findAll({
+  //     attributes: ["brand"],
+  //     group: ["brand"],
+  //   });
+  // };
 }
 
-module.exports = new CarsService()
+module.exports = new CarsService();
